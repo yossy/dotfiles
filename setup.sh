0fc32ee002b383e33416e82f9be696c86a398953
yossy0806 <t.yoshimura0806@gmail.com>
2021-01-24 18:44:25 +0900

    add zprezto install

--- setup.sh
+++ setup.sh
@@ -1,13 +1,30 @@
-#!/bin/sh
+#!/bin/zsh
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
-DOT_FILES=(.vimrc)
+DOT_FILES=(.vimrc .zshenv .zprofile .zshrc .zlogin .zlogout .zpreztorc .zprezto)
 
+# add symlink dotfiles
 for file in ${DOT_FILES[@]}
 do
-  ln -s $HOME/dotfiles/$file $HOME/$file
+  ln -sf $HOME/dotfiles/$file $HOME/$file
 done
 
+# add submodule
+git submodule update --init --recursive
+
+# prezto
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+ ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# change shell
+chsh -s $(which zsh)
+
+source $SCRIPT_DIR/.zshrc
+source $SCRIPT_DIR/.zpreztorc
+
+# vscodeのinstall
 "${SCRIPT_DIR}/.vscode/vscode_install.sh"
 
 echo "Done"
