c5b73dcb830b635319868a55d506a8db9deeedb5
yossy <t.yoshimura0806@gmail.com>
2021-03-28 18:21:48 +0900

    vscodeの設定を更新

--- vscode/vscode_install.sh
+++ vscode/vscode_install.sh
@@ -10,12 +10,12 @@ rm "$VSCODE_SETTING_DIR/keybindings.json"
 ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
 # install extention
-cat "${SCRIPT_DIR}/extensions" | while read line
+cat "${SCRIPT_DIR}/extensions.txt" | while read line
 do
  code --install-extension $line
 done
 
-code --list-extensions > "${SCRIPT_DIR}/extensions"
+code --list-extensions > "${SCRIPT_DIR}/extensions.txt"
 
 # VSCodeVimでNomalMode切り替え時に英数入力に変更するためim-selectをinstallする。
 curl -Ls https://raw.githubusercontent.com/daipeihust/im-select/master/install_mac.sh | sh
