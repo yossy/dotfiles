567c8af90cd88d507801bf44b90a70caa5a74870
yossy <t.yoshimura0806@gmail.com>
2021-05-03 21:46:39 +0900

    add ricty patch script

---
+++ ricty-patch.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+fontforge -script ./font-patcher ~/Library/Fonts/Ricty\ Bold\ for\ Powerline.ttf --fontawesome --fontlinux --octicons --powersymbols --powerline --powerlineextra --material --weather
+fontforge -script ./font-patcher ~/Library/Fonts/Ricty\ Discord\ Bold\ for\ Powerline.ttf --fontawesome --fontlinux --octicons --powersymbols --powerline --powerlineextra --material --weather
+fontforge -script ./font-patcher ~/Library/Fonts/Ricty\ Discord\ Regular\ for\ Powerline.ttf --fontawesome --fontlinux --octicons --powersymbols --powerline --powerlineextra --material --weather
+fontforge -script ./font-patcher ~/Library/Fonts/Ricty\ Regular\ for\ Powerline.ttf --fontawesome --fontlinux --octicons --powersymbols --powerline --powerlineextra --material --weather
+fontforge -script ./font-patcher ~/Library/Fonts/Ricty-Bold.ttf --fontawesome --fontlinux --octicons --powersymbols --powerline --powerlineextra --material --weather
+fontforge -script ./font-patcher ~/Library/Fonts/Ricty-Regular.ttf --fontawesome --fontlinux --octicons --powersymbols --powerline --powerlineextra --material --weather
+fontforge -script ./font-patcher ~/Library/Fonts/RictyDiscord-Bold.ttf --fontawesome --fontlinux --octicons --powersymbols --powerline --powerlineextra --material --weather
+fontforge -script ./font-patcher ~/Library/Fonts/RictyDiscord-Regular.ttf --fontawesome --fontlinux --octicons --powersymbols --powerline --powerlineextra --material --weather
