0902c46aab6b2cfbadb11e230736e1a563e98db6
yossy <t.yoshimura0806@gmail.com>
2024-01-14 14:11:06 +0900

    Add command for copy filename including relative path for neo-tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,20 @@ return {
         hide_dotfiles = false,
         hide_gitignored = false,
       },
+      commands = {
+        -- Copy filename including relative path
+        copy_file_name = function(state)
+          local node = state.tree:get_node()
+          local full_path = node.path
+          local relative_path = full_path:sub(#state.path + 2)
+          vim.fn.setreg("*", relative_path, "c")
+        end,
+      },
+      window = {
+        mappings = {
+          ["Y"] = "copy_file_name",
+        },
+      },
     },
   },
 }
